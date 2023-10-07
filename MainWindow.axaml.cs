0cf0b76658fa62bf716ba062a3167d496fd9284f
Zaphkie1ka <skyrim.skyrim.1001@gmail.com>
2023-10-07 20:19:13 +0700

    добавлена кнопка для перехода на второе окно

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,11 +1,19 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace ZapApp1;
 
 public partial class MainWindow : Window
 {
+    SecondWindow window2 = new SecondWindow();
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void Btn1_Click(object? sender, RoutedEventArgs e)
+    {
+        this.Close();
+        window2.Show();
     }
 }
