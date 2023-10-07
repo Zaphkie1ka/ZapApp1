b36961096780a2e4b321e65f904b47f9f24108d4
Zaphkie1ka <skyrim.skyrim.1001@gmail.com>
2023-10-07 19:55:19 +0700

    Добавлено второе окно

---
+++ Models/Passengers.cs
@@ -0,0 +1,11 @@
+﻿namespace ZapApp1.Models;
+
+public class Passengers
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public string Sex { get; set; }
+    public int Age { get; set; }
+    public int Passport { get; set; }
+    public string Nationality { get; set; }
+}
---
+++ SecondWindow.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ZapApp1;
+
+public partial class SecondWindow : Window
+{
+    public SecondWindow()
+    {
+        InitializeComponent();
+    }
+}
